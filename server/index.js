27ae16d4fff1e8e57375e257ba1a1b5b16c4a5c2
sharanyeah <kallemsharanyareddy21@gmail.com>
2025-10-21 12:14:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
